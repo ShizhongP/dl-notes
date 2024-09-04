8dbd324416088466ad7d47460af321936f7f8e1f
psz <2422158541@qq.com>
2024-09-04 16:46:30 +0800

    add kv cache

--- src/model/llama/attention.py
+++ src/model/llama/attention.py
@@ -22,8 +22,8 @@ class Transformer_Attention(nn.Module):
         
         self.output = nn.Linear(dim_v,dim_embeding,bias=False)
         
-    def forward(self,x):
-        batch_size,sequence_length, dim_embedding = x.shape
+    def forward(self,x,start_pos,mask):
+        bs,seq_len, dim_embedding = x.shape
         
         assert self.dim_embeding == dim_embedding
         
@@ -33,15 +33,21 @@ class Transformer_Attention(nn.Module):
         # batch_size , sequence_length , dim_v
         v =self.linear_v(x)
         
+         
         if self.kv_cache:
-            k = torch.cat([self.kv_cache_k,k],dim=1)
-            v = torch.cat([self.kv_cache_v,v],dim=1)
-            self.kv_cache_k = k[:,-1:].detach()
-            self.kv_cache_v = v[:,-1:].detach()
-        
+            self.kv_cache_k[:bs, start_pos:start_pos+seq_len] = k
+            self.kv_cache_v[:bs, start_pos:start_pos+seq_len] = v
+            
+            k = self.kv_cache_k[:bs, :start_pos+seq_len]
+            v = self.kv_cache_v[:bs, :start_pos+seq_len]
+            
+             
         dist = torch.matmul(q,k.transpose(-2,-1)) * self._normal_factor
-        
         weight = dist.detach()
+        
+        if mask:
+            dist = dist + mask
+            # dist = dist.masked_fill(mask, -1e9)
         dist = torch.softmax(dist,dim=-1)
         
         atten_score = torch.matmul(dist,v)
@@ -50,24 +56,3 @@ class Transformer_Attention(nn.Module):
         return atten_score, weight
 
 # Llama2 Attention with kv-cache and  GQA
-class Attention(nn.Module):
-    def __init__(self, dim_embedding:int,dim_qk:int,dim_v:int,kv_cache:bool=True):
-        super(Attention, self).__init__()
-        self.dim_embedding=dim_embedding
-        self.dim_qk=dim_qk
-        self.dim_v=dim_v
-        
-        self.linear_q=nn.Linear(dim_embedding,dim_qk,bias=False)
-        self.linear_k=nn.Linear(dim_embedding,dim_qk,bias=False)
-        self.linear_v=nn.Linear(dim_embedding,dim_v,bias=False)
-        
-        self._normal_factor =  (self.dim_qk//2) ** -0.5
-        
-        self.kv_cache=kv_cache
-        if kv_cache:
-            self.kv_cache_k = torch.zeros((1, 1, dim_qk))
-            self.kv_cache_v = torch.zeros((1, 1, dim_v))
-        
-        self.output = nn.Linear(dim_v,dim_embedding,bias=False)
-    def forward(self,x):
-        pass
